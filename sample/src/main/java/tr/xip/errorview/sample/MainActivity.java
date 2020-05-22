52a1d58e11f7a9457494752b9be2813d805c9ff6
CrazyXacker <sashamonster@gmail.com>
2020-05-22 18:41:20 +0300

    Some redesign

--- sample/src/main/java/tr/xip/errorview/sample/MainActivity.java
+++ sample/src/main/java/tr/xip/errorview/sample/MainActivity.java
@@ -18,34 +18,21 @@ public class MainActivity extends Activity {
         final ErrorView mErrorView = findViewById(R.id.error_view);
         mErrorView.setTint(Color.rgb(100, 25, 0));
 
-        mErrorView.setOnRetryListener(new ErrorView.RetryListener() {
-            @Override
-            public void onRetry() {
-                Toast.makeText(MainActivity.this, R.string.info_retrying, Toast.LENGTH_SHORT).show();
+        mErrorView.setOnRetryListener(() -> {
+            Toast.makeText(MainActivity.this, R.string.info_retrying, Toast.LENGTH_SHORT).show();
 
-                new Handler().postDelayed(() -> mErrorView.setTitle(getString(R.string.error_title_damn))
-                        .setTitleColor(getResources().getColor(R.color.apptheme_primary))
-                        .setSubtitle(getString(R.string.error_subtitle_failed_one_more_time))
-                        .setRetryButtonText(getString(R.string.error_view_retry))
-                        .setSecondSubtitle("Subtitle 2")
-                        .setSecondRetryButtonText("Retry 2")
-                        .setThirdSubtitle("Subtitle 3")
-                        .setThirdRetryButtonText("Retry 3")
-                        .showSecondSubtitle(true)
-                        .showThirdSubtitle(true)
-                        .showSecondRetryButton(true)
-                        .showThirdRetryButton(true), 5000);
-            }
-
-            @Override
-            public void onSecondRetry() {
-
-            }
-
-            @Override
-            public void onThirdRetry() {
-
-            }
+            new Handler().postDelayed(() -> mErrorView.setTitle(getString(R.string.error_title_damn))
+                    .setTitleColor(getResources().getColor(R.color.apptheme_primary))
+                    .setSubtitle(getString(R.string.error_subtitle_failed_one_more_time))
+                    .setRetryButtonText(getString(R.string.error_view_retry))
+                    .setSecondSubtitle("Subtitle 2")
+                    .setSecondRetryButtonText("Retry 2")
+                    .setThirdSubtitle("Subtitle 3")
+                    .setThirdRetryButtonText("Retry 3")
+                    .showSecondSubtitle(false)
+                    .showThirdSubtitle(true)
+                    .showSecondRetryButton(true)
+                    .showThirdRetryButton(true), 2000);
         });
     }
 }
